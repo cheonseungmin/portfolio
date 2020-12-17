03986dfc2b277aae867343e24742b33691915a81
Cheon seungmin <dudghsx12@gmail.com>
2020-12-17 22:46:32 +0900

    set a background image

--- jsx/index/App.jsx
+++ jsx/index/App.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import styledComponents from 'styled-components';
 
 import Header from './Header.jsx';
 import Contents from './Contents.jsx';
 
 import { AppContext } from './AppContext.jsx';
 
+const Wrapper = styledComponents.div`
+    background-image: url('../img/index/body.png');
+`
+
 const App = () => {
 
     const [contentType, setContentType] = useState('About Me');
 
     return (
-        <div>
+        <Wrapper>
             <AppContext.Provider value={ { contentType, setContentType } }>
                 <Header />
                 <Contents />
             </AppContext.Provider>
-        </div>
+        </Wrapper>
     )
 }
 
